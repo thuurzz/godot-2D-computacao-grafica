3788a5da15a10f47de55f0c279c2ae927ba36001
Arthur Vinicius <thuur.vss@gmail.com>
2022-08-20 22:18:21 -0300

    Musica de fundo e atalho teclado

--- Main.gd
+++ Main.gd
@@ -11,6 +11,8 @@ func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 
 func new_game():
 	score = 0
@@ -19,6 +21,7 @@ func new_game():
 	$HUD.update_score(score)
 	$HUD.show_message("Get Ready")
 	get_tree().call_group("mobs", "queue_free")
+	$Music.play()
 
 func _on_ScoreTimer_timeout():
 	score += 1
